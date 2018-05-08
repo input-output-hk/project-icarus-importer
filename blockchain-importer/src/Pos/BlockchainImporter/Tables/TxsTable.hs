c4a307062e3925d4169002a2bc2c51dd554e6748
Javier Diaz <javier.diaz@iohk.io>
2018-05-08 20:07:44 -0300

    Finished implementation of insertion in tx_details. Started working on rollbacking data.

--- blockchain-importer/src/Pos/BlockchainImporter/Tables/TxsTable.hs
+++ blockchain-importer/src/Pos/BlockchainImporter/Tables/TxsTable.hs
@@ -5,33 +5,30 @@
 {-# LANGUAGE OverloadedStrings     #-}
 {-# LANGUAGE TemplateHaskell       #-}
 
+-- TODO: Add documentation.
+
 module Pos.BlockchainImporter.Tables.TxsTable
-(
-  insertTx
-) where
+  ( insertTx
+  , deleteTx
+  ) where
 
 import           Control.Monad (void)
-import qualified Data.List.NonEmpty as NE (toList)
 import           Data.Profunctor.Product.TH (makeAdaptorAndInstance)
-import           Data.Time.Clock (UTCTime)
 import qualified Database.PostgreSQL.Simple as PGS
 import           Opaleye
 import           Pos.BlockchainImporter.Core (TxExtra (..))
+import qualified Pos.BlockchainImporter.Tables.TxDetailsTable as TDT (insertTxDetails)
 import           Pos.BlockchainImporter.Tables.Utils (hashToString)
-import           Pos.BlockchainImporter.Web.ClientTypes (convertTxOutputs, convertTxOutputsMB)
 import           Pos.Core (timestampToUTCTimeL)
-import           Pos.Core.Txp (Tx (..), TxId)
+import           Pos.Core.Txp (Tx (..))
 import           Pos.Crypto (hash)
 import           Universum
 
+data TxRowPoly a b c = TxRow { trHash     :: a
+                             , trBlockNum :: b
+                             , trTime     :: c
+                             } deriving (Show)
 
-data TxRowPoly a b c = TxRow {
-    trHash     :: a
-  , trBlockNum :: b
-  , trTime     :: c
-  } deriving (Show)
-
-type TxRow = TxRowPoly TxId Word64 UTCTime
 type TxRowPGW = TxRowPoly (Column PGText) (Column (Nullable PGInt8)) (Column (Nullable PGTimestamptz))
 type TxRowPGR = TxRowPoly (Column PGText) (Column (Nullable PGInt8)) (Column (Nullable PGTimestamptz))
 
@@ -43,28 +40,29 @@ txsTable = Table "txs" (pTxs TxRow { trHash     = required "hash"
                                    , trTime     = required "time"
                                    })
 
--- FIXME: Replace with inserting a list of txs?
 insertTx :: PGS.Connection -> Tx -> TxExtra -> Word64 -> IO ()
-insertTx conn tx txExtra blockHeight = do
-  putStrLn $ "************* blockHeight: " ++ show blockHeight
-  putStrLn $ "************* tx: " ++ show tx
-  putStrLn $ "************* txExtra: " ++ show txExtra
+insertTx conn tx txExtra blockHeight = PGS.withTransaction conn $ do
+  insertTxHeader conn tx txExtra blockHeight
+  TDT.insertTxDetails conn tx txExtra
+
+insertTxHeader :: PGS.Connection -> Tx -> TxExtra -> Word64 -> IO ()
+insertTxHeader conn tx txExtra blockHeight = do
+  -- FIXME: Remove, only for debugging purposes.
+  --putStrLn $ "************* blockHeight: " ++ show blockHeight
+  --putStrLn $ "************* tx: "          ++ show tx
+  --putStrLn $ "************* txExtra: "     ++ show txExtra
   void $ runInsertMany conn txsTable [row]
   where
-    row = TxRow {
-      trHash     = pgString $ hashToString (hash tx)
-    , trBlockNum = toNullable $ pgInt8 $ fromIntegral blockHeight
-    , trTime     = maybeToNullable utcTime
-    }
+    row = TxRow { trHash     = pgString $ hashToString (hash tx)
+                , trBlockNum = toNullable $ pgInt8 $ fromIntegral blockHeight
+                , trTime     = maybeToNullable utcTime
+                }
     utcTime = pgUTCTime . (^. timestampToUTCTimeL) <$> teReceivedTime txExtra
 
-insertTxDetails :: PGS.Connection -> Tx -> TxExtra -> IO ()
-insertTxDetails conn tx txExtra blockHeight = do
-  putStrLn $ "--------- txInputs: " ++ show txInputs
-  putStrLn $ "--------- txOutputs: " ++ show txOutputs
-  --void $ runInsertMany conn txsDetailsTable rows
+deleteTx :: PGS.Connection -> Tx -> IO ()
+deleteTx conn tx = do
+  -- FIXME: Remove, only for debugging purposes.
+  putStrLn $ "///////////// tx: " ++ show tx
+  void $ runDelete conn txsTable $ \row -> trHash row .== txHash
   where
-    rows = [] -- TODO: Code
-    txInputs      = map (fmap (second mkCCoin)) txInputsMB -- TODO: Filter Nothing and convert to plain list
-    txInputsMB    = convertTxOutputsMB $ map (fmap toaOut) $ NE.toList $ teInputOutputs txExtra
-    txOutputs     = convertTxOutputs . NE.toList $ _txOutputs tx
+    txHash = pgString $ hashToString (hash tx)
